0c0cc52f4fd1d45c803569cc50c1bc1a55bb0ff6
j <j@localhost.com>
2020-05-24 16:07:34 -0500

    init commit

--- sifter_app/urls.py
+++ sifter_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/stay_alive', views.stay_alive, name='stay_alive'),
+    path('stay_alive/', views.stay_alive, name='stay_alive'),
 ]
